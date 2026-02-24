da77f47e050b2dfccc14a147b91a60d6c05b995c
Riha879 <rihansaifi0687@gmail.com>
2026-02-24 15:58:37 +0530

    data type

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 console.log("hello world");
 document.write("Hello World");
+var num = 10;
+        console.log(num);
+        console.log(typeof(num));
+        num = false;
+        console.log(typeof(num));
+        num= undefined;
+        console.log(typeof(num));
+
+const sym1 = Symbol(4)
+console.log(sym1);
+const sym2 = Symbol(3)    
+console.log(sym2);
+if(sym1 == sym2) {
+    console.log("true")
+}    else {
+    console.log("false")
+}
+// var a = 100; var b = 12; var c = 10;
+// var linebreak = "<br/>";
+// var name = (age<18)?"too young":"old enough";
