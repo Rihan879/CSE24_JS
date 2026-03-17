863d759ff0a74ffd2dd59439968c17eb3cb9b5db
Riha879 <rihansaifi0687@gmail.com>
2026-03-17 15:35:18 +0530

    function

--- script.js
+++ script.js
@@ -1,49 +1,71 @@
 // console.log("hello world");
 // document.write("Hello World");
-var num = 10;
-        console.log(num);
-        console.log(typeof(num));
-        num = false;
-        console.log(typeof(num));
-        num= undefined;
-        console.log(typeof(num));
+// var num = 10;
+//         console.log(num);
+//         console.log(typeof(num));
+//         num = false;
+//         console.log(typeof(num));
+//         num= undefined;
+//         console.log(typeof(num));
 
-const sym1 = Symbol(4)
-console.log(sym1);
-const sym2 = Symbol(3)    
-console.log(sym2);
-if(sym1 == sym2) {
-    console.log("true")
-}    else {
-    console.log("false")
-}
-var a = 100; var b = 12; var c = 10;
-var linebreak = "<br/>";
-document.write("a+b=");
-result=a+b;
-document.write(result);
-document.write(linebreak);
+// const sym1 = Symbol(4)
+// console.log(sym1);
+// const sym2 = Symbol(3)    
+// console.log(sym2);
+// if(sym1 == sym2) {
+//     console.log("true")
+// }    else {
+//     console.log("false")
+// }
+// var a = 100; var b = 12; var c = 10;
+// var linebreak = "<br/>";
+// document.write("a+b=");
+// result=a+b;
+// document.write(result);
+// document.write(linebreak);
 
-document.write("a-b=");
-result=a-b;
-document.write(result);
-document.write(linebreak);
+// document.write("a-b=");
+// result=a-b;
+// document.write(result);
+// document.write(linebreak);
 
-document.write("a*b=");
-result=a*b;
-document.write(result);
-document.write(linebreak);
+// document.write("a*b=");
+// result=a*b;
+// document.write(result);
+// document.write(linebreak);
 
-console.log("a/b=");
-result=a/b;
-console.log(result);
+// console.log("a/b=");
+// result=a/b;
+// console.log(result);
 
 
 // let age = 15;
 // var vote = (age<18)?"too young":"old enough";
-var count;
-document.write("starting loop" + "<br/>");
-for(count = 0; count<10; count++) {
-    document.write("current count :" + count);
+// var count;
+// document.write("starting loop" + "<br/>");
+// for(count = 0; count<10; count++) {
+//     document.write("current count :" + count);
+
+// }
+var grade='A';
+document.write("Entering switch block<br/>")
+function myfunction(){
+    alert("Hello World")
+}
+function sayHello(name,age){
+    document.write(name+" is"+age+" year old")
+}
+var res=mul(12,30);
+function mul(){
+    Document.write(mul);
+}
+function add(x,y){
+    return x+y;
+}
+var person= new Object();{
+    person.name="Rihan";
+    person.age=20;
+    person.address={Masuri};
+    person['address']['home No']=484;
 
 }
