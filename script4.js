5a4ec1abfc63c1821de4d80edd774e3bf5825e0d
Riha879 <rihansaifi0687@gmail.com>
2026-06-20 20:56:16 +0530

    DOM example

--- script4.js
+++ script4.js
@@ -1,7 +1,19 @@
-function changeContent() {
-    let heading = document.getElementById("heading");
+// function changeContent() {
+//     let heading = document.getElementById("heading");
 
-    heading.innerHTML = "Text Changed!";
-    heading.style.color = "blue";
-    heading.style.fontSize = "40px";
+//     heading.innerHTML = "Text Changed!";
+//     heading.style.color = "blue";
+//     heading.style.fontSize = "40px";
+// }
+
+function addItem() {
+    let input = document.getElementById("itemInput");
+    let list = document.getElementById("itemList");
+
+    let newItem = document.createElement("li");
+    newItem.textContent = input.value;
+
+    list.appendChild(newItem);
+
+    input.value = "";
 }
