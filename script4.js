ca73981fd156970f3279743744e535c8dc958369
Riha879 <rihansaifi0687@gmail.com>
2026-06-20 20:53:16 +0530

    DOM example

---
+++ script4.js
@@ -0,0 +1,7 @@
+function changeContent() {
+    let heading = document.getElementById("heading");
+
+    heading.innerHTML = "Text Changed!";
+    heading.style.color = "blue";
+    heading.style.fontSize = "40px";
+}
